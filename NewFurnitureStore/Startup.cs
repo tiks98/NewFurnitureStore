8ad96da871956ccb76aae4763da42dddc6deac0d
Tilak Suthar <tilaksuthar98@gmail.com>
2019-08-10 15:07:58 -0400

    Add project files.

---
+++ NewFurnitureStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewFurnitureStore.Startup))]
+namespace NewFurnitureStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
