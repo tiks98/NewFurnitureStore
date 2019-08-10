1955dad5dc2564b97c8c099925bd1d59b25dbc19
Tilak Suthar <tilaksuthar98@gmail.com>
2019-08-10 15:27:23 -0400

    Added Models

---
+++ NewFurnitureStore/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using FurnitureStore.Models;
+
+namespace FurnitureStore.Models
+{
+    public class Product
+    {
+  
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public WoodType WoodType { get; set; }
+
+        public int WoodTypeId { get; set; }
+
+        public string Discription { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Color { get; set; }
+
+        public double Price { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+        public int ProductTypeId { get; set; }
+        [NotMapped]
+        public HttpPostedFileBase ImageFile { get; set; }
+
+        public Store Store { get; set; }
+
+        public int StoreId { get; set; }
+        public int Stock { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ NewFurnitureStore/Models/ProductType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FurnitureStore.Models
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
