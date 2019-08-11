8d8063209b53f3f6f7e0f71ccd0bc4ac261f567f
Rupal <200393408@student.georgianc.on.ca>
2019-08-10 22:12:17 -0400

    Created Product and Product Type API

---
+++ NewFurnitureStore/Controllers/API/ProductTypesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FurnitureStore.Models;
+using NewFurnitureStore.Models;
+
+namespace NewFurnitureStore.Controllers.API
+{
+    public class ProductTypesController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ProductTypes
+        public IQueryable<ProductType> GetProductTypes()
+        {
+            return db.ProductTypes;
+        }
+
+        // GET: api/ProductTypes/5
+        [ResponseType(typeof(ProductType))]
+        public IHttpActionResult GetProductType(int id)
+        {
+            ProductType productType = db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productType);
+        }
+
+        // PUT: api/ProductTypes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductType(int id, ProductType productType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productType.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(productType).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ProductTypes
+        [ResponseType(typeof(ProductType))]
+        public IHttpActionResult PostProductType(ProductType productType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ProductTypes.Add(productType);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = productType.Id }, productType);
+        }
+
+        // DELETE: api/ProductTypes/5
+        [ResponseType(typeof(ProductType))]
+        public IHttpActionResult DeleteProductType(int id)
+        {
+            ProductType productType = db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            db.ProductTypes.Remove(productType);
+            db.SaveChanges();
+
+            return Ok(productType);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            return db.ProductTypes.Count(e => e.Id == id) > 0;
+        }
+    }
+}
